fb733fd9642ab514f738aaeba8afe3d82875ffbf
Ridoy Roy <ridoyroycse53engg@gmail.com>
2022-02-17 13:48:01 +0600

    Error Message Added for string and Negative value

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,39 @@
 // Calculation star here 
 function calculate() {
-    const food = document.getElementById('food');
-    const rent = document.getElementById('rent');
-    const cloth = document.getElementById('cloth');
+    const food = document.getElementById('food').value;
+    const rent = document.getElementById('rent').value;
+    const cloth = document.getElementById('cloth').value;
     const expenses = document.getElementById('expenses');
-    const income = document.getElementById('income');
+    const income = document.getElementById('income').value;
     const balance = document.getElementById('balance');
-    const totalExpenses = parseFloat(food.value) + parseFloat(rent.value) + parseFloat(cloth.value);
+    const totalExpenses = parseFloat(food) + parseFloat(rent) + parseFloat(cloth);
+    // Error Message
+    const fail = document.getElementById('notify-fail');
+    const success = document.getElementById('notify-success');
+    if (isNaN(income) || isNaN(food) || isNaN(rent) || isNaN(cloth)) {
+        success.style.display = 'none';
+        fail.style.display = 'block';
+
+    }
+    else {
+        success.style.display = 'block';
+        fail.style.display = 'none';
+
+    }
+
+    // Error Message for negative Number
+    if (income < 0 || food < 0 || rent < 0 || cloth < 0) {
+        success.style.display = 'none';
+        fail.style.display = 'block';
+
+    }
+    else {
+        success.style.display = 'block';
+        fail.style.display = 'none';
+
+    }
+
     expenses.innerText = totalExpenses;
-    newbalance = parseFloat(income.value) - totalExpenses;
+    newbalance = parseFloat(income) - totalExpenses;
     balance.innerText = newbalance;
 }
